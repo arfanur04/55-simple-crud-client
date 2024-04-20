f7dc1dec8b8c992fab9dac4cc04d75b69b33b5b8
arfanur04 <arfanur04@gmail.com>
2024-04-20 23:22:01 +0600

    55-3 Create Client slide Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,48 @@
 import "./App.css";
 
 function App() {
+	const handleAddUser = (e) => {
+		e.preventDefault();
+		const form = e.target;
+		const name = form.name.value;
+		const email = form.email.value;
+		const user = { name, email };
+		console.log(`user:`, user);
+
+		fetch(`http://localhost:5000/users`, {
+			method: "POST",
+			headers: {
+				"content-type": "application/json",
+			},
+			body: JSON.stringify(user),
+		})
+			.then((res) => res.json())
+			.then((data) => console.log(data));
+	};
+
 	return (
 		<>
 			<h1>Simple CRUD</h1>
+			<form onSubmit={handleAddUser}>
+				<input
+					type="text"
+					name="name"
+					placeholder="Name"
+					id=""
+				/>
+				<br />
+				<input
+					type="email"
+					name="email"
+					placeholder="Email"
+					id=""
+				/>
+				<br />
+				<input
+					type="submit"
+					value={"Add User"}
+				/>
+			</form>
 		</>
 	);
 }
